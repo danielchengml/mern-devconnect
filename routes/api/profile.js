13e8f2d907692c00fa7b26893dd0a9cc328360a0
Daniel Cheng <danielchengml@gmail.com>
2018-04-19 09:21:52 -0700

    add get user by handle/ id and get all method

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -34,10 +34,60 @@ router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => 
         .catch(err => res.json(404).json(err));
 });
 
+// @route   POST api/profile/all
+// @desc    Get All Profiles
+// @access  Public
+router.get('/all', (req, res) => {
+    Profile.find()
+        .populate('user', ['name','avatar'])
+        .then(profiles => {
+            if(!profiles) {
+                errors.noprofile = 'There are no profiles';
+                return res.status(404).json(errors)
+            }
+
+            res.json(profiles);
+        })
+        .catch(err => 
+            res.status(404).json({ profile: 'There are no profiles' })
+        );
+});
+
 // @route   POST api/profile/handle/:handle
 // @desc    Get Profile by Handle
 // @access  Public
+router.get('/handle/:handle', (req, res) => {
+    const errors = {};
 
+    Profile.findOne({ handle: req.params.handle })
+        .populate('user', ['name','avatar'])
+        .then(profile => {
+            if(!profile) {
+                errors.noprofile = 'There is no profile for this user';
+                res.status(404).json(errors);
+            }
+            res.json(profile);
+        })
+        .catch(err => res.status(404).json(err));
+});
+
+// @route   POST api/profile/user/:user_id
+// @desc    Get Profile by User ID
+// @access  Public
+router.get('/user/:user_id', (req, res) => {
+    const errors = {};
+
+    Profile.findOne({ user: req.params.user_id })
+        .populate('user', ['name','avatar'])
+        .then(profile => {
+            if(!profile) {
+                errors.noprofile = 'There is no profile for this user';
+                res.status(404).json(errors);
+            }
+            res.json(profile);
+        })
+        .catch(err => res.status(404).json(err));
+});
 
 // @route   POST api/profile
 // @desc    Create or Edit user's profile
