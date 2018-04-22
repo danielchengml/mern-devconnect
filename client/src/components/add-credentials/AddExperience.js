d7a87a5e1defbea32b351967604c5b596b94255a
Daniel Cheng <danielchengml@gmail.com>
2018-04-22 08:06:22 -0700

    add Experience adds to db

--- client/src/components/add-credentials/AddExperience.js
+++ client/src/components/add-credentials/AddExperience.js
@@ -4,6 +4,7 @@ import TextFieldGroup from "../common/TextFieldGroup";
 import TextAreaFieldGroup from "../common/TextAreaFieldGroup";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
+import { addExperience } from "../../actions/profileActions";
 
 class AddExperience extends Component {
   constructor(props) {
@@ -25,10 +26,26 @@ class AddExperience extends Component {
     this.onCheck = this.onCheck.bind(this);
   }
 
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
+  }
+
   onSubmit(e) {
     e.preventDefault();
 
-    console.log("submit");
+    const expData = {
+      company: this.state.company,
+      title: this.state.title,
+      location: this.state.location,
+      from: this.state.from,
+      to: this.state.to,
+      current: this.state.current,
+      description: this.state.description
+    };
+
+    this.props.addExperience(expData, this.props.history);
   }
 
   onChange(e) {
@@ -122,7 +139,7 @@ class AddExperience extends Component {
                   <input
                     type="submit"
                     value="submit"
-                    className="btn btn-light btn-block mt-4"
+                    className="btn btn-info btn-block mt-4"
                   />
                 </div>
               </form>
@@ -140,8 +157,11 @@ AddExperience.propTypes = {
 };
 
 const mapStateToProps = state => ({
+  addExperience: PropTypes.func.isRequired,
   profile: state.profile,
   errors: state.errors
 });
 
-export default connect(mapStateToProps)(withRouter(AddExperience));
+export default connect(mapStateToProps, { addExperience })(
+  withRouter(AddExperience)
+);
