0368eead16926bda3122b04b301ef07032c13190
Daniel Cheng <danielchengml@gmail.com>
2018-04-23 07:45:53 -0700

    delete comment works

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -6,11 +6,13 @@ import {
   GET_POSTS,
   GET_POST,
   POST_LOADING,
+  CLEAR_ERRORS,
   DELETE_POST
 } from "./types";
 
 // Add Post
 export const addPost = postData => dispatch => {
+  dispatch(clearErrors());
   axios
     .post("/api/posts", postData)
     .then(res =>
@@ -118,6 +120,7 @@ export const setPostLoading = () => {
 
 // Add Comment
 export const addComment = (postId, commentData) => dispatch => {
+  dispatch(clearErrors());
   axios
     .post(`/api/posts/comment/${postId}`, commentData)
     .then(res =>
@@ -132,4 +135,29 @@ export const addComment = (postId, commentData) => dispatch => {
         payload: err.response.data
       })
     );
+};
+
+// Delete Comment
+export const deleteComment = (postId, commentId) => dispatch => {
+  axios
+    .delete(`/api/posts/comment/${postId}/${commentId}`)
+    .then(res =>
+      dispatch({
+        type: GET_POST,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Clear errors
+export const clearErrors = () => {
+  return {
+    type: CLEAR_ERRORS
+  };
 };
